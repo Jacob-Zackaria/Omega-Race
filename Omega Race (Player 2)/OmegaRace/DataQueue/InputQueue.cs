1839a7f32b9f363fbc13bff5acd814378ed6a435
Jacob Zackaria <44209093+Jacob-Zackaria@users.noreply.github.com>
2021-07-28 11:26:13 -0500

    Add files via upload

---
+++ Omega Race (Player 2)/OmegaRace/DataQueue/InputQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaRace
+{
+    class InputQueue
+    {
+        private static InputQueue instance = null;
+        public static InputQueue Instance()
+        {
+            if (instance == null)
+            {
+                instance = new InputQueue();
+            }
+            return instance;
+        }
+
+        Queue<MixedMessage> pInputQueue;
+
+        private InputQueue()
+        {
+            pInputQueue = new Queue<MixedMessage>();
+        }
+
+        public static void AddToQueue(MixedMessage msg)
+        {
+            instance.pInputQueue.Enqueue(msg);
+        }
+
+        public static void Process()
+        {
+            while (instance.pInputQueue.Count > 0)
+            {
+                MixedMessage msg = instance.pInputQueue.Dequeue();
+
+                GameManager.RecieveMessage(msg);
+            }
+        }
+    }
+}
